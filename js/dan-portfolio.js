1ec11c9b0cb36f753cb99625c7b6445106e61080
Danilo de Moura Chenchi <dan.chenchi@gmail.com>
2017-04-04 10:11:52 -0300

    Updated performance
    Updated performance and both arrays of content

--- js/dan-portfolio.js
+++ js/dan-portfolio.js
@@ -5,18 +5,16 @@
 */
 
 /* Each image have a respective title. So it's a 1-to-1 relation of the two arrays */
-var images_array = ["lotr wallpaper.jpg",
+var images_array = [
 					"You gonna love it HD.png",
-					"lotr wallpaper2.jpg",
-					"Rogue-One-A-Star-Wars-Story-2016-Wallpaper-03854.jpg",
-					"Skyrim.jpg",
-					"StormGiant_Gallery_Thumb.jpg"];
-var titles_array = ["Battle against the Witchking",
+					"Quote cite.png",
+					"hello.png",
+					];
+var titles_array = [
 					"You gonna love IT",
-					"The Fellowship",
-					"Rogue One Death Trooper",
-					"Skyrim",
-					"Storm King's Thunder"];
+					"Quote someone today",
+					"Hello World!",
+					];
 
 /* Add all the images registered in the array to the gallery */
 function initGallery() {
@@ -24,12 +22,15 @@ function initGallery() {
 	for (i = 0; i < images_array.length; i++) {
 		addThumbnail( images_array[i], titles_array[i]);
 	}
+	
+	// Reset materialboxed
+	$('.materialboxed').materialbox();
 }
 
 /*
 Thumbnail
-<div class="col s4">
-	<div class="card small">
+<div class="col s12 m6 l4">
+	<div class="card">
 		<div class="card-image">
 			<img class="materialboxed" data-caption="title" src="path/image.ext">
 		</div>
@@ -47,8 +48,7 @@ function addThumbnail(image_name, title) {
 	var card_content = $("<div></div>");
 
 // Add classes to style the card
-	column.addClass("col s4");
-	//card_small.addClass("card small");
+	column.addClass("col s12 m6 l4");
 	card_small.addClass("card");
 	card_image.addClass("card-image");
 	card_content.addClass("card-content");
@@ -59,21 +59,16 @@ function addThumbnail(image_name, title) {
 	thumb_image.attr("data-caption", title);
 	thumb_image.attr("src", "imgs/" + image_name);
 	
+// Add title
+	var thumb_title = $("<span></span>");
+	thumb_title.addClass("card-title");
+	thumb_title.text(title);
+	
+// Structure the card html tree bottom-up
 	card_image.append(thumb_image);
-	
-// Add content
-	var thumb_content = $("<span></span>");
-	thumb_content.addClass("card-title");
-	thumb_content.text(title);
-	
-	card_image.append(thumb_content);
-
-// Structure the card html tree
+	card_image.append(thumb_title);
 	card_small.append(card_image);
 	column.append(card_small);
 	
 	$("#thumb-galley").append(column);
-
-// Reset materialboxed to amplify the thumbnails
-	$('.materialboxed').materialbox();
 }
